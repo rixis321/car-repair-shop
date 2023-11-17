b45dbacf47a8ec13fa83c9fa6a348d0744516c1c
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-11-17 07:54:49 +0100

    zmiana generowania kodu na liczby i litery tylko

--- backend/src/main/java/com/example/backend/utils/AccessCodeGenerator.java
+++ backend/src/main/java/com/example/backend/utils/AccessCodeGenerator.java
@@ -5,12 +5,12 @@ import java.util.stream.Collectors;
 
 public class AccessCodeGenerator {
 
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String generateCode(){
         SecureRandom secureRandom = new SecureRandom();
-        StringBuilder sb = new StringBuilder(6);
-        return secureRandom.ints(6,0,CHARACTERS.length())
+        StringBuilder sb = new StringBuilder(8);
+        return secureRandom.ints(8,0,CHARACTERS.length())
                 .mapToObj(CHARACTERS::charAt)
                 .collect(StringBuilder::new,StringBuilder::append,StringBuilder::append)
                 .toString();
