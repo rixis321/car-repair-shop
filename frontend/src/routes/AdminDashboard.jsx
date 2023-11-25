575decb26e3290f8792b1668c579ae79bcd8cf9b
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-11-25 15:13:27 +0100

    added security check

--- frontend/src/routes/AdminDashboard.jsx
+++ frontend/src/routes/AdminDashboard.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import AdminNavbar from "../components/navbar/AdminNavbar.jsx";
 import AdminSidebar from "../components/sidebar/AdminSidebar.jsx";
+import AuthContext from "../security/AuthProvider.jsx";
+import {useNavigate} from "react-router";
 
 const AdminDashboard = () => {
+    const{auth} = useContext(AuthContext)
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.accessToken) {
+            navigate("/login");
+        }
+    }, [auth.accessToken]);
     return (
         <>
             <AdminNavbar/>
